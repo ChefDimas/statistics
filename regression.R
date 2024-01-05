4fdfb3a7bd465437b415e98f9c8d78f9d39c1160
ChefDimas <dumitrulunik@gmail.com>
2024-01-05 22:38:41 +0100

    Changes

--- regression.R
+++ regression.R
@@ -1,2 +1,14 @@
 library(readxl)
-data <- read_excel("./data/Moving.xls")
+data <- read_excel("./data/Moving.xlsx")
+model <- lm(Hours ~ Feet, data = data)
+print(summary(model))
+
+
+plot(data$Hours, data$Feet)
+abline(model, col = "red")
+plot(model)
+
+
+new_data <- data.frame(Feet = 800)
+predictions <- predict(model, new_data)
+print(predictions)
